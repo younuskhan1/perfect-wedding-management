0e3ccdc9c73ae4003bcdceb296632990d592bf33
younuskhan1 <md.younuskhan412@gmail.com>
2024-03-15 12:01:30 +0600

    the copyWrite line is added and year is added dynamically

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -5,26 +5,31 @@ import appStore from "../../assets/app-play-store.png";
 import microsoft from "../../assets/microsoft.png";
 
 const Footer = () => {
+
+    let year = new Date();
+    let currentYear = year.getFullYear();
+    // console.log(currentYear);
+
     return (
-            <div className="footer-container">
-                <div className="footer-elements">
-                    <div className="office-address-division">
-                        <h3 className="office-heading">Office Address</h3>
-                        <div>
-                                <p>Level-6, 56, New York Center, Dilkusha, Dhaka.</p>
-                                <p>Support: Web@WeddingEventManagement.com</p>
-                                <p>Helpline: 0139359023539</p>
-                                <p>(Available: Sat - Thu, 10:00 AM To 7:00 PM)</p>
-                        </div>
+        <div className="footer-container">
+            <div className="footer-elements">
+                <div className="office-address-division">
+                    <h3 className="office-heading">Office Address</h3>
+                    <div>
+                        <p>Level-6, 56, New York Center, Dilkusha, Dhaka.</p>
+                        <p>Support: Web@WeddingEventManagement.com</p>
+                        <p>Helpline: 0139359023539</p>
+                        <p>(Available: Sat - Thu, 10:00 AM To 7:00 PM)</p>
+                    </div>
                     </div>
                     <div className="useful-links">
-                            <h3 className="useful-heading">Useful Available Links</h3>
-                            <ul>
-                                <NavLink to="/aboutUs"><li>About Us</li></NavLink>
-                                <NavLink to="/contact"><li>Contact</li></NavLink>
-                                <NavLink to="/booking"><li>Booking</li></NavLink>
-                                <NavLink to="/login"><li>Login</li></NavLink>
-                            </ul>
+                        <h3 className="useful-heading">Useful Available Links</h3>
+                        <ul>
+                            <NavLink to="/aboutUs"><li>About Us</li></NavLink>
+                            <NavLink to="/contact"><li>Contact</li></NavLink>
+                            <NavLink to="/booking"><li>Booking</li></NavLink>
+                            <NavLink to="/login"><li>Login</li></NavLink>
+                        </ul>
                     </div>
                     <div className="social-media">
                         <h3>Follow Us</h3>
@@ -33,19 +38,20 @@ const Footer = () => {
                         <i className="fa-brands fa-linkedin"></i>
                         <i className="fa-brands fa-youtube"></i>
                         <div className="google-play-app-store-container">
-                                <div className="google-play-store">
-                                    <img src={googlePlayStore} alt="" />
-                                </div>
-                                <div className="app-store">
-                                    <img src={appStore} alt="" />
-                                </div>
+                            <div className="google-play-store">
+                                <img src={googlePlayStore} alt="" />
+                            </div>
+                            <div className="app-store">
+                                <img src={appStore} alt="" />
+                            </div>
                         </div>
                         <div className="microsoft">
-                                <img src={microsoft} alt="" />
+                            <img src={microsoft} alt="" />
                         </div>
-                    </div>
                 </div>
             </div>
+            <p className="copy-write-text"><span>&#169;</span> Perfect Wedding Management {currentYear}</p>
+        </div>
     );
 };
 
