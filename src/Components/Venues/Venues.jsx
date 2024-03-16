fbadec4ef8026dd7d3f10203b71b355484fc6cf8
younuskhan1 <md.younuskhan412@gmail.com>
2024-03-16 14:11:00 +0600

    the venues section is completed

--- src/Components/Venues/Venues.jsx
+++ src/Components/Venues/Venues.jsx
@@ -12,6 +12,7 @@ const Venues = () => {
     return (
         <div>
             <h1 className="venues-heading">Our Available Venues</h1>
+            <p className="venues-description">Certain types of venues offer overnight accommodations, for example, while others focus solely on the event space. While some venues are flexible, with the ability to accommodate a variety of event styles, others are designed to accommodate a specific event niche, such as weddings or government events.</p>
             <div className="venue-container">
                 <div className="venue-card">
                     <img src={sonargaon} alt="" />
@@ -19,19 +20,39 @@ const Venues = () => {
                     <p>107 Kazi Nazrul Islam Ave, Dhaka 1215</p>
                 </div>
                 <div className="venue-card">
-                    <img src={sonargaon} alt="" />
-                    <h4>Pan Pacific Sonargaon</h4>
-                    <p>107 Kazi Nazrul Islam Ave, Dhaka 1215</p>
+                    <img src={intercontinental} alt="" />
+                    <h4>Hotel Intercontinental</h4>
+                    <p> G, 1 Minto Rd, Dhaka 1000</p>
                 </div>
                 <div className="venue-card">
-                    <img src={sonargaon} alt="" />
-                    <h4>Pan Pacific Sonargaon</h4>
-                    <p>107 Kazi Nazrul Islam Ave, Dhaka 1215</p>
+                    <img src={radisson} alt="" />
+                    <h4>Radisson Hotels</h4>
+                    <p>Airport Rd, Dhaka 1206</p>
                 </div>
                 <div className="venue-card">
-                    <img src={sonargaon} alt="" />
-                    <h4>Pan Pacific Sonargaon</h4>
-                    <p>107 Kazi Nazrul Islam Ave, Dhaka 1215</p>
+                    <img src={westine} alt="" />
+                    <h4>The Westin Dhaka</h4>
+                    <p>Main Gulshan Avenue, Plot-01 Rd 45, Dhaka 1212</p>
+                </div>
+                <div className="venue-card">
+                    <img src={grandSultan} alt="" />
+                    <h4>Grand Sultan Tea Resort & Golf</h4>
+                    <p>Apt: D2, House108, Road 8, Block C, Dhaka 1213</p>
+                </div>
+                <div className="venue-card">
+                    <img src={momoIn} alt="" />
+                    <h4>Momo Inn Park & Resort</h4>
+                    <p>Rangpur Road, Bogura 5800</p>
+                </div>
+                <div className="venue-card">
+                    <img src={laMaridian} alt="" />
+                    <h4>Le Méridien Hotels & Resorts</h4>
+                    <p>79/A Commercial Area Airport Rd, Dhaka 1229</p>
+                </div>
+                <div className="venue-card">
+                    <img src={regency} alt="" />
+                    <h4>Dhaka Regency Hotel & Resort</h4>
+                    <p>Airport Road | Nikunja 2 Dhaka 1229 | Bangladesh</p>
                 </div>
             </div>
         </div>
